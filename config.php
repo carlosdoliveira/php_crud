578e745dcbfdb11ec75dc2743d3645b017c7bc1c
carlosdoliveira <cdaniel.oliveira@outlook.com>
2021-08-31 20:57:23 -0300

    Adicionando configs do webserver

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'ubuntu3');
+define('DB_SERVER', 'vm-database-prd-1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_PASSWORD', 'Password1');
+define('DB_NAME', 'ecommerce');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
